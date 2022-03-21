756416e26905d08aabaf992a0560194cf22ced36
vincent villaluna <villalunavincent@gmail.com>
2022-03-21 17:50:31 +0800

    add profile

---
+++ pages/profile.js
@@ -0,0 +1,29 @@
+import { withSession } from '../lib/server/withSession';
+import { login } from '../lib/lens-api/login';
+
+
+export default function Profile({ user }) {
+	const onClick = async () => {
+		const accessTokens = await login(user?.address);
+		console.log(accessTokens);
+	}
+	return (
+		<div>
+			<h1>{ user?.address }</h1>
+			<button onClick={() => onClick()}>Create Profile</button>
+		</div>
+	)
+}
+
+export const getServerSideProps = withSession(({ req, res }) => {
+	if (!req?.session?.user) {
+		res.setHeader('location', '/login');
+		res.statusCode = 302;
+		res.end();
+	}
+	return {
+		props: {
+			user: req?.session?.user,
+		},
+	}
+})
