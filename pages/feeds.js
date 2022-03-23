39fd2f7737cc5f89a0d78e7eec109455523fe2cf
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 <cristianachavez214@gmail.com>
2022-03-23 12:48:14 +0800

    :art: apply formatting on pages

--- pages/feeds.js
+++ pages/feeds.js
@@ -1,37 +1,43 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import style from '../styles/Feeds.module.css';
 import { withSession } from '../lib/server/withSession';
 import { explore } from '../lib/lens-api/explore-publications';
 
 export default function Feeds({ publications }) {
+	console.log(publications);
 	return (
 		<div>
 			<h1>Feeds</h1>
-		{
-			publications.items.map(item => (
+			{publications.items.map((item) => (
 				<div className={style.publication} key={item.id}>
 					<div className={style.profile}>
-				{ item.profile?.picture?.original?.url ? (
-					<>
-						<div>
-							<img src={item.profile?.picture?.original?.url} width={80} height={80} />
-						</div>
-						<span><strong>{ item?.profile?.name ? item.profile.name : 'unknown' }</strong> - ({ item?.profile?.ownedBy})</span>
-					</>
-				) : null
-				}
+						{item.profile?.picture?.original?.url ? (
+							<>
+								<div>
+									<img
+										src={item.profile?.picture?.original?.url}
+										width={80}
+										height={80}
+										alt={item?.profile?.name ? item.profile.name : 'unknown'}
+									/>
+								</div>
+								<span>
+									<strong>{item?.profile?.name ? item.profile.name : 'unknown'}</strong> - (
+									{item?.profile?.ownedBy})
+								</span>
+							</>
+						) : null}
 					</div>
-					<h1>{ item.metadata.content }</h1>
-					<span className={style.type}>{ item.__typename }</span>
-					<span className={style.created}>Created: { item.createdAt }</span>
+					<h1>{item.metadata.content}</h1>
+					<span className={style.type}>{item.__typename}</span>
+					<span className={style.created}>Created: {item.createdAt}</span>
 				</div>
-			))
-		}
+			))}
 		</div>
-	)
+	);
 }
 
-export const getServerSideProps = withSession( async ({ req, res }) => {
+export const getServerSideProps = withSession(async ({ req, res }) => {
 	if (!req?.session?.user) {
 		res.setHeader('location', '/login');
 		res.statusCode = 302;
@@ -40,7 +46,7 @@ export const getServerSideProps = withSession( async ({ req, res }) => {
 	const { explorePublications } = await explore();
 	return {
 		props: {
-			publications: explorePublications
+			publications: explorePublications,
 		},
-	}
-})
+	};
+});
